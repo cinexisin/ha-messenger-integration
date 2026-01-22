231a160bbd9187e32ca94ce1f2070a3577ccdbbc
cinexisin <cinexisin@gmail.com>
2026-01-22 11:37:17 +0530

    Create const.py

---
+++ cinexis/const.py
@@ -0,0 +1,8 @@
+DOMAIN = "cinexis"
+
+CONF_ADDON_SLUG = "addon_slug"
+CONF_NOTIFY_SERVICE = "notify_service"
+CONF_DEFAULT_CHANNEL = "default_channel"
+
+DEFAULT_NOTIFY_SERVICE = "cinexis"
+DEFAULT_CHANNEL = "whatsapp"
